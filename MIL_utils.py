0e342786e4e5c572c9e82ed6581c6361a1363434
clferma1 <clferma1@i3b.upv.es>
2023-11-02 17:06:34 +0100

    added set_seed function

--- MIL_utils.py
+++ MIL_utils.py
@@ -54,6 +54,15 @@ import torch.optim as optim
 # Old GNN Aggregaion Methods (might delete later)
 from torch_geometric.nn import SAGEConv, ClusterGCNConv, global_max_pool, max_pool, dense_diff_pool, DenseSAGEConv
 
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
 def get_train_test_val_ids(dataset_splitting_path):
     """
     Retrieves the train, test, and validation patient IDs from the BCNB Challenge dataset splitting.
