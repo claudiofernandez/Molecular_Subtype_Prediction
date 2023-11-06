386728c0ddf5a70167f674c3647411636984e44d
clferma1 <clferma1@i3b.upv.es>
2023-11-06 10:23:19 +0100

    fix data path slurm

--- code/main_GCNs.py
+++ code/main_GCNs.py
@@ -11,7 +11,7 @@ def main_gcns_cv(args):
 
     # Set up directories depending on where this program is executed
     if args.where_exec == "slurm_nas":
-        args.gnrl_data_dir = '/workspace/NASFolder'
+        args.gnrl_data_dir = os.path.join('/workspace/NASFolder', args.dataset)
         output_directory = os.path.join(args.gnrl_data_dir, "output")
     elif args.where_exec == "slurm_dgx":
         #TODO: change so it is not directly BCNB here
@@ -204,100 +204,6 @@ if __name__ == '__main__':
 
     # Graph Configuration
     parser.add_argument('--knn', type=int, default=8, help='# of K nearest neighbours for graph creation.')  # 8, 19, 25
-    parser.add_argument('--edge_agg', type=str, default='spatial', help="What edge relationship to use for aggregation.")  # It is possible to use spatial or latent edge aggregation
-    parser.add_argument('--include_edge_features',  default=False, type=lambda x: (str(x).lower() == 'true'), help='Include edge_features (euclidean dist) in the graph')
-
-    # GCN Configuration
-    parser.add_argument('--gcn_layer_type', type=str, default="GCNConv", help='Type of GCN layers to use.')  # ['GCNConv', 'SAGEConv', 'GATConv', 'GINConv', 'GENConv', 'GraphConv']
-    parser.add_argument('--num_gcn_layers', type=int, default=4, help='# of GCN layers to use.')  # [4, 5]
-    parser.add_argument('--graph_pooling', type=str, default="mean", help="mean, max, attention")  # TODO: CHECK Graph pooling
-    parser.add_argument('--drop_out', default=True, type=lambda x: (str(x).lower() == 'true'), help='Enable dropout (p=0.25)')
-
-    args = parser.parse_args()
-    main_gcns_cv(args)
-
-    ##########################
-    # CREATE ARGUMENT PARSER #
-    ##########################
-    parser = argparse.ArgumentParser()
-
-    #MLFlow configuration
-    parser.add_argument("--mlflow_experiment_name", default="[02_11_2023] GCNs", type=str,  help='Name for experiment in MLFlow')
-    parser.add_argument('--mlflow_server_url', type=str, default="http://158.42.170.104:8002", help='URL of MLFlow DB')
-
-    # General Configuration
-    parser.add_argument('--where_exec', type=str, default="local", help="slurm_dgx, slurm_nas, dgx_gpu or local")
-    parser.add_argument('--preload_data', default=True, type=lambda x: (str(x).lower() == 'true'), help="Load data on RAM memory.")
-    parser.add_argument('--num_workers', type=int, default=200, help= "number of CPU processes or threads used for data loading and preprocessing during training.")
-    parser.add_argument('--seed', type=int, default=42, help="seed that will be used for reproducibility.")
-
-    # Mode Configuration
-    parser.add_argument('--pred_column', type=str, default="Molecular subtype", help='Column from GT dataframe that we want to predict')
-    parser.add_argument('--training_type', type=str, default="CV", help='CV or full_dataset')
-    parser.add_argument('--dataset', type=str, default="BCNB", help='BCNB or CLARIFY_DB')
-    parser.add_argument('--shuffle', default=False, type=lambda x: (str(x).lower() == 'true'), help="Load data on RAM memory.")
-    parser.add_argument("--aggregation", default='Patch_GCN_offline', type=str)  # max, mean, TransMIL, TransMIL_pablo, Patch_GCN_online, Patch_GCN_offline
-    parser.add_argument("--pred_mode", default="OTHERvsTNBC", type=str)  # "LUMINALAvsLAUMINALBvsHER2vsTNBC", "LUMINALSvsHER2vsTNBC", "OTHERvsTNBC"
-    parser.add_argument("--epochs", default=100, type=int, help="Number of epochs for training")
-    parser.add_argument("--loss_function", default="cross_entropy", type=str)  # cross_entropy, kll
-    parser.add_argument("--lr", default=0.0001, type=float, help="Learning rate")  #  [0.00002, 0.00001, 0.0002, 0.0001]
-    parser.add_argument("--criterion", default='auc', type=str, help="Metric to keep the best model: 'auc', 'f1'")
-    parser.add_argument("--optimizer_type", default='adam', type=str)  # sgd, adam, lookahead_radam
-    parser.add_argument("--optimizer_weight_decay", default=0.00001, type=float)
-    parser.add_argument("--alpha_ce", default=1., type=float)
-    parser.add_argument("--early_stopping", default=True, type=lambda x: (str(x).lower() == 'true'))
-    parser.add_argument("--scheduler", default=True, type=lambda x: (str(x).lower() == 'true'))
-    parser.add_argument("--virtual_batch_size", default=1, type=int)
-
-    # Graph Configuration
-    parser.add_argument('--knn', type=int, default=19, help='# of K nearest neighbours for graph creation.')  # 8, 19, 25
-    parser.add_argument('--edge_agg', type=str, default='spatial', help="What edge relationship to use for aggregation.")  # It is possible to use spatial or latent edge aggregation
-    parser.add_argument('--include_edge_features',  default=False, type=lambda x: (str(x).lower() == 'true'), help='Include edge_features (euclidean dist) in the graph')
-
-    # GCN Configuration
-    parser.add_argument('--gcn_layer_type', type=str, default="GCNConv", help='Type of GCN layers to use.')  # ['GCNConv', 'SAGEConv', 'GATConv', 'GINConv', 'GENConv', 'GraphConv']
-    parser.add_argument('--num_gcn_layers', type=int, default=4, help='# of GCN layers to use.')  # [4, 5]
-    parser.add_argument('--graph_pooling', type=str, default="mean", help="mean, max, attention")  # TODO: CHECK Graph pooling
-    parser.add_argument('--drop_out', default=True, type=lambda x: (str(x).lower() == 'true'), help='Enable dropout (p=0.25)')
-
-    args = parser.parse_args()
-    main_gcns_cv(args)
-
-    ##########################
-    # CREATE ARGUMENT PARSER #
-    ##########################
-    parser = argparse.ArgumentParser()
-
-    #MLFlow configuration
-    parser.add_argument("--mlflow_experiment_name", default="[02_11_2023] GCNs", type=str,  help='Name for experiment in MLFlow')
-    parser.add_argument('--mlflow_server_url', type=str, default="http://158.42.170.104:8002", help='URL of MLFlow DB')
-
-    # General Configuration
-    parser.add_argument('--where_exec', type=str, default="local", help="slurm_dgx, slurm_nas, dgx_gpu or local")
-    parser.add_argument('--preload_data', default=True, type=lambda x: (str(x).lower() == 'true'), help="Load data on RAM memory.")
-    parser.add_argument('--num_workers', type=int, default=200, help= "number of CPU processes or threads used for data loading and preprocessing during training.")
-    parser.add_argument('--seed', type=int, default=42, help="seed that will be used for reproducibility.")
-
-    # Mode Configuration
-    parser.add_argument('--pred_column', type=str, default="Molecular subtype", help='Column from GT dataframe that we want to predict')
-    parser.add_argument('--training_type', type=str, default="CV", help='CV or full_dataset')
-    parser.add_argument('--dataset', type=str, default="BCNB", help='BCNB or CLARIFY_DB')
-    parser.add_argument('--shuffle', default=False, type=lambda x: (str(x).lower() == 'true'), help="Load data on RAM memory.")
-    parser.add_argument("--aggregation", default='Patch_GCN_offline', type=str)  # max, mean, TransMIL, TransMIL_pablo, Patch_GCN_online, Patch_GCN_offline
-    parser.add_argument("--pred_mode", default="OTHERvsTNBC", type=str)  # "LUMINALAvsLAUMINALBvsHER2vsTNBC", "LUMINALSvsHER2vsTNBC", "OTHERvsTNBC"
-    parser.add_argument("--epochs", default=100, type=int, help="Number of epochs for training")
-    parser.add_argument("--loss_function", default="cross_entropy", type=str)  # cross_entropy, kll
-    parser.add_argument("--lr", default=0.0001, type=float, help="Learning rate")  #  [0.00002, 0.00001, 0.0002, 0.0001]
-    parser.add_argument("--criterion", default='auc', type=str, help="Metric to keep the best model: 'auc', 'f1'")
-    parser.add_argument("--optimizer_type", default='adam', type=str)  # sgd, adam, lookahead_radam
-    parser.add_argument("--optimizer_weight_decay", default=0.00001, type=float)
-    parser.add_argument("--alpha_ce", default=1., type=float)
-    parser.add_argument("--early_stopping", default=True, type=lambda x: (str(x).lower() == 'true'))
-    parser.add_argument("--scheduler", default=True, type=lambda x: (str(x).lower() == 'true'))
-    parser.add_argument("--virtual_batch_size", default=1, type=int)
-
-    # Graph Configuration
-    parser.add_argument('--knn', type=int, default=25, help='# of K nearest neighbours for graph creation.')  # 8, 19, 25
     parser.add_argument('--edge_agg', type=str, default='spatial', help="What edge relationship to use for aggregation.")  # It is possible to use spatial or latent edge aggregation
     parser.add_argument('--include_edge_features',  default=False, type=lambda x: (str(x).lower() == 'true'), help='Include edge_features (euclidean dist) in the graph')
 
